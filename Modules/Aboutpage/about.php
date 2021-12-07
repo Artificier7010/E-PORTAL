e485d9ffbce57e518130bc10a0b1fb4b60574b9f
dhananjay sahu <dhananjaysahu500007@gmail.com>
2021-12-07 18:00:30 +0530

    about page

---
+++ Modules/Aboutpage/about.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+    <title>About</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap');
+
+        * {
+            padding: 0;
+            margin: 0;
+            font-family: 'Raleway', sans-serif;
+            box-sizing: border-box;
+        }
+
+        html {
+            height: 100%;
+            min-height: 720px;
+        }
+
+        body {
+            width: 100%;
+            height: 100%;
+            min-height: 720px;
+            /*background-color: skyblue;*/
+            background-color: rgb(189, 189, 189);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            overflow: auto;
+        }
+
+        .main {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            width: 99%;
+            height: 98vh;
+            background: whitesmoke;
+            border-radius: 10px;
+            overflow: hidden;
+
+        }
+
+        .back-circle {
+
+            position: absolute;
+            top: -81px;
+            left: -81px;
+            height:clamp(150px, 45vw, 200px);
+            width: clamp(150px, 45vw, 200px);
+            border-radius: 50%;
+            background: #ffb47f;
+            color: white;
+        }
+
+        .back-circle i {
+            position: relative;
+            top: 64%;
+            left: 64%;
+            font-size: 2rem;
+        }
+
+        .text-container {
+            /*background-color: greenyellow;*/
+            width: 75%;
+            height: 80%;
+            /* border: 1px solid black; */
+            max-width: 1500px;
+            overflow: auto;
+
+        }
+
+        .logo-txt {
+            width: 150px;
+            height: 50px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .logo-txt img {
+            width: 50px;
+            height: 50px;
+        }
+
+        .box-img {
+            height: 52%;
+            width: 100%;
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .benefits {
+            position: relative;
+            height: 100%;
+            width: 90%;
+
+
+        }
+        @media  screen and (max-width:991px) {
+            .main{
+                width: 100%;
+                height: 100%;
+                border-radius: 0px;
+            }
+            
+        }
+    </style>
+</head>
+
+<body>
+    <div class="main">
+        <a href="../../index.php" class="back-circle">
+            <i class="fas fa-arrow-left "></i>
+        </a>
+        <div class="text-container about-text">
+            <div class="logo-txt">
+                <img src="../../Assets/lightbglogo.png" alt="">
+                <h1>Wiscore</h1>
+            </div>
+            <hr size="2px" color="rgb(255 180 127)">
+            <br>
+            <h2>Online Exam Portals: What Are They?</h2>
+            <br>
+            <p>An online exam portal is a platform that allows examiners to conduct tests online. Most exam portals are web-based and do not require test takers to download an app or a program to take an exam.</p>
+            <br>
+            <p>Online exam portals are also known as computer-based assessments, test portals, examination software, and exam systems, among others.</p>
+            <br>
+            <p>Back then, the main purpose of online exam portals was to reduce the time it takes to make, administer, and take exams. These days, online exam portals have become the most reasonable way for examiners to conduct tests.</p>
+            <br>
+            <p>Education technology investments were already high at US$18.6 billion pre-pandemic. But, the online education market is projected to reach US$350 billion by 2025. We’re already seeing a dramatic increase in the demand for video conferencing, virtual tutoring, online learning management, school management systems, and online exam portals.</p>
+            <br>
+            <p>Community quarantine guidelines had schools and universities close their doors for face-to-face classes. These guidelines also transformed physical classes into virtual ones. Now, students can go to classes and take their exams all from the safety and comfort of their own homes.</p>
+            <br>
+            <center>
+                <h2>Basic Features of Online Examination Software</h2>
+            </center>
+            <hr size="1px" color="rgb(255 180 127)">
+            <br>
+            <p>Online exam systems need to have a range of features that can help educators and examiners conduct online exams that can provide accurate assessments. When you’re looking at different test portals, remember to look for the following basic features:</p>
+            <br>
+            <h2>Test Management</h2>
+            <br>
+            <p>How many questions per page should the test have? Would you allow test takers to go back to previous questions? Would you allow test stops and, if so, how long could a test stop be? Can your students skip questions, and will they be allowed to round back to the questions they skipped?</p>
+            <br>
+            <p>Online exams can be very complex, and you’re going to want to set some exam rules if you want your test to be effective. There are so many things to consider when administering an easy-to-use test, and you should work with an online exam portal that lets you do things in a way that proves most effective.</p>
+            <br>
+            <h2>Test Authoring</h2>
+            <br>
+            <p>Online tests can come in different forms. You can administer multiple-choice, enumeration, true or false, essay, matching, and other types of online tests.</p>
+            <br>
+            <p> Wiscore, an all-in-one school management system, offers a Question Bank feature. This feature lets you create and store test items that you can prepare for future exams. It stores all types of exam questions too.</p>
+            <br>
+            <p>Teachers should be able to use whatever test type that best suits the online courses and their students. Descriptive and objective tests both have their benefits.</p>
+            <br>
+            <p>When you’re looking at a test portal to try, think of all the types of tests you’re likely to administer. If you’re a school administrator looking at an integrated exam management feature, you might want to get feedback from your teachers about the types of tests they’re using or are planning to use.</p>
+            <br>
+            <p>Also, consider conducting online training sessions to have a better grasp of how these online learning exam portals work for your institution.</p>
+            <br>
+            <h2>User Management</h2>
+            <br>
+            <p>User profiles offer bits of information that can help students and teachers. This is why user management is a basic online exam portal feature.</p>
+            <br>
+            <p>Students can access their profiles and see which tests they’re supposed to take and when. This allows them to better manage their time and decide which subjects to review first. Their profiles can even show test results.</p>
+            <br>
+            <p>Teachers can see who is yet to take their tests, and they can send reminders to their students. If a test doesn’t require manual grading, test results may be available as soon as a student finishes a test.</p>
+            <br>
+            <p>Test analytics can prove very helpful to teachers in finding out which topics their students are having a hard time with. This data is essential in a learning management system.</p>
+            <br>
+            <h2>Security</h2>
+            <br>
+            <p>Tests should be secure to safeguard against cheating and unauthorized access. Online exam portals should have security measures in place to ensure that individual test environments are safe.</p>
+            <br>
+            <p>Some test portals record students’ browser activities, especially social media interactions. Some tests may even require students to turn on their cameras, so you can better monitor them during important exams. AI-powered tools can detect suspicious browser activities and lock students out of exams. If you think they’ve been locked out by mistake, you can just let students back in manually.</p>
+            <br>
+            <center>
+                <h2>How to Choose the Best Online Testing Software</h2>
+            </center>
+            <hr size="1px" color="rgb(255 180 127)">
+            <br>
+            <p>Whether you’re looking for an easy-to-use test portal for personal use, or you’re in charge of deciding on an online exam portal for the whole school, there are a few things you should consider.</p>
+            <br>
+            <p>What factors should you consider in purchasing an online exam portal?</p>
+            <br>
+            <h2>1. Functionality and Compatibility</h2>
+            <br>
+            <p>Can an online exam portal disrupt a school’s workflow? Of course, it can</p>
+            <br>
+            <p>Compatibility ensures that the exam portal won’t render your current workflow useless and vice versa.</p>
+            <br>
+            <p>Do you integrate an exam portal that works with your current workflow? Or do you choose one that will bring about a change in the workflow?</p>
+            <br>
+            <p>Functionality ensures that teachers and students will be able to use the portal as intended. Pilot programs are great since they allow you to see results without abrupt, large-scale, and sometimes irreversible changes.</p>
+            <br>
+            <h2>2. Value and Cost</h2>
+            <br>
+            <p>Determining your budget allows you to narrow down your choices into only the exam portals you can afford. It allows you to see whether you can get premium features or premium versions of exam portals you’re eyeing.</p>
+            <br>
+            <p>If the only exam portals you can get are those that aren’t aligned with your goals and required functionalities, it wouldn’t be a sensible purchase even if it’s within your budget.</p>
+            <br>
+            <p>It’s easy to get a trial for an online exam portal free of charge. Most developers offer free trials for online examination software. But, even if you choose a free online exam software, chances are you’re still going to invest time, money, and a lot of effort in implementing the new software system.</p>
+            <br>
+            <p>Notice, however, that we won’t include pricing details for the best online exam portals in this list. Most online exam software solutions offer free demos, but pricing usually depends on the size of the institution. Some developers charge per exam or per user. Some exam portals, especially the open-sourced ones, won’t offer dedicated support for free users.</p>
+            <br>
+            <h2>3. Credibility and Customer Service</h2>
+            <br>
+            <p>When you’re looking at different test portals, you’re going to come across reviews and testimonials about what makes each test portal better than the others. Reviews are great sources of information about what test portals can offer. They also give you an insight as to what types of organizations are using a specific test portal.</p>
+            <br>
+            <p>It’s important to work with developers or vendors who can guide you in the installation and implementation of these online testing software programs. They should also be able to handle training if you’re dealing with more complex features or more extensive school management solutions.</p>
+            <br>
+            <p>But, don’t just look at what an online exam portal can offer. Look at what the developers can offer as well.</p>
+            <br>
+            <p>What kind of support can they provide? How can you reach them? What is their availability? Customer service is part of a software solution vendor’s job. You can ask them these questions to gauge how good their customer service is.</p>
+            <br>
+            <center>
+                <h2>Benefits of Online Exam Software Solutions</h2>
+            </center>
+            <hr size="1px" color="rgb(255 180 127)">
+            <br>
+            <p>We’ve already given you the list of the best online exam portals. But, you may still be on the fence about investing in an exam software solution. Now, let’s learn more about the benefits of online exam portals to students, teachers, and schools.</p>
+            <br>
+            <p>1.Students only need an internet-connected computer or smart device to register and take online exams. The school won’t need to spend on printing and paper for answer sheets and questionnaires.</p>
+            <br>
+            <p>2.Schools may designate remote proctors and monitors, or just use online exam portals with security features to prevent cheating and malpractice.</p>
+            <br>
+            <p>3.Students may not be required to come to different exam locations to take their exams. Online exams are safer for students, teachers, and their families.</p>
+            <br>
+            <p>4.Teachers and test authors can make tests within minutes instead of hours. Question banks, templates, and re-usable questions make for faster and easier test creation.</p>
+            <br>
+            <p>5.Students who live in areas that experience regular or rotational power interruptions won’t need to worry. Test answers may be backed up or saved in real-time.</p>
+            <br>
+            <p>6.Students, especially those with bad handwriting, may find essay-type questions in pen-and-paper exams quite intimidating. They won’t have to worry about writing legibly in online exams.</p>
+            <br>
+            <p>7.Teachers won’t need to check objective test papers one by one. Online exam portals can automatically generate test results as soon as a student finishes the exam.</p>
+            <br>
+            <p>8.Teachers can use generated reports to modify or improve future tests, lesson plans, and teaching methods.</p>
+        </div>
+        <script src="../../jquery-3.6.0.js"></script>
+</body>
+
+</html>
