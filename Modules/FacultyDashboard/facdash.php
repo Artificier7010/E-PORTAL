758855247c13ffd697f6daba3c3648f3733747b9
animesh verma <animeshverma161@gmail.com>
2021-12-30 01:01:19 +0530

    table added

--- Modules/FacultyDashboard/facdash.php
+++ Modules/FacultyDashboard/facdash.php
@@ -33,6 +33,15 @@ $anss = mysqli_query($connect, $sql2);
 
 ?>
 
+<!-- User Attendence details Data -->
+<?php
+
+$attsql = "SELECT * FROM regist WHERE chekin='checked'";
+
+$attdet = mysqli_query($connect, $attsql);
+
+?>
+
 
 
 <!-- User result -->
@@ -123,7 +132,8 @@ if (isset($_POST['date'])) {
       box-sizing: border-box;
       font-family: "Raleway";
     }
-    html{
+
+    html {
       height: auto;
     }
 
@@ -431,6 +441,7 @@ if (isset($_POST['date'])) {
       margin-bottom: 50px;
 
     }
+
     .container {
       width: 100%;
       background-color: #fff;
@@ -616,58 +627,70 @@ if (isset($_POST['date'])) {
     /* Table Styling */
     /* *********************** */
 
-    #customers {
-      font-family: Arial, Helvetica, sans-serif;
-      border-collapse: collapse;
+    table {
       width: 100%;
-
+      border-spacing: 0;
+      border-radius: 1em;
+      overflow: hidden;
     }
 
-    #customers td,
-    #customers th {
+    thead {
+      visibility: hidden;
+      position: absolute;
+      width: 0;
+      height: 0;
+    }
+
+    th {
+      background: rgb(255 180 127);
+      color: #fff;
+    }
+
+    td:nth-child(1) {
+      background: rgb(255 180 127);
+      color: #fff;
+      border-radius: 1em 1em 0 0;
+    }
+
+    th,
+    td {
+      padding: 1em;
+    }
+
+    tr,
+    td {
+      display: block;
+    }
+
+    td {
+      position: relative;
+    }
+
+    td::before {
+      content: attr(data-label);
+      position: absolute;
+      left: 0;
+      padding-left: 1em;
+      font-weight: 600;
+      font-size: .9em;
+      text-transform: uppercase;
+    }
+
+    tr {
+      margin-bottom: 1.5em;
       border: 1px solid #ddd;
-      padding: 8px;
+      border-radius: 1em;
+      text-align: right;
     }
 
-    #customers tr:nth-child(even) {
-      background-color: #f2f2f2;
+    tr:last-of-type {
+      margin-bottom: 0;
     }
 
-    #customers tr:hover {
-      background-color: #ddd;
+    td:nth-child(n+2):nth-child(odd) {
+      background-color: #ffe2c4;
     }
 
-    #customers th {
-      padding-top: 12px;
-      padding-bottom: 12px;
-      text-align: left;
-      background: linear-gradient(-135deg, #71b7e6, #9b59b6);
-      color: white;
-    }
-
-    .tru {
-      color: #03ff18;
-
-    }
-
-    .tru2 {
-      color: #91ff9a;
-
-    }
-
-    .fal {
-      color: #ff4a4a;
-    }
-
-    .result {
-      width: 300px;
-      height: 80px;
-      border-radius: 10px;
-      border: 1px solid gray;
-      background: #ebffeb;
-      text-align: center;
-      margin: 20px auto;
-    }
 
     /* === HEADING STYLE #3 === */
     .three h1 {
@@ -811,6 +834,11 @@ if (isset($_POST['date'])) {
       padding: 0 10px;
       box-shadow: 5px 5px 15px rgba(54, 162, 235, 0.2);
       color: black;
+
+    }
+
+    /* Attendence Status */
+    .attstatus{
 
     }
 
@@ -959,6 +987,58 @@ if (isset($_POST['date'])) {
         flex-direction: column;
       }
     }
+
+    /* Table Media Screen */
+    @media only screen and (min-width: 768px) {
+
+      table {
+        max-width: 1200px;
+        margin: 0 auto;
+        border: 1px solid #ddd;
+      }
+
+      thead {
+        visibility: visible;
+        position: relative;
+      }
+
+      th {
+        text-align: left;
+        text-transform: uppercase;
+        font-size: .9em;
+      }
+
+      tr {
+        display: table-row;
+        border: none;
+        border-radius: 0;
+        text-align: left;
+      }
+
+      tr:nth-child(even) {
+        background-color: #ffe2c4;
+      }
+
+      td {
+        display: table-cell;
+      }
+
+      td::before {
+        content: none;
+      }
+
+      td:nth-child(1) {
+        background: transparent;
+        color: #444;
+        border-radius: 0;
+      }
+
+      td:nth-child(n+2):nth-child(odd) {
+        background-color: transparent;
+      }
+
+
+    }
   </style>
 </head>
 
@@ -1087,66 +1167,45 @@ if (isset($_POST['date'])) {
 
     <div class="addcat">
       <div class="container">
-      <table>
-  
-  <thead>
-    <th>name</th>
-    <th>war</th>
-    <th>ba</th>
-    <th>obp</th>
-    <th>slg</th>
-    <th>ops</th>
-    <th>rbi</th>
-    <th>hr</th>
-  </thead>
-  
-  <tbody>
-    <tr>
-      <td data-label="name">Ken Griffey Jr.</td>
-      <td data-label="war">83.8</td>
-      <td data-label="ba">.284</td>
-      <td data-label="obp">.370</td>
-      <td data-label="slg">.583</td>
-      <td data-label="ops">.907</td>
-      <td data-label="rbi">1836</td>
-      <td data-label="hr">630</td>
-    </tr>
-      
-      <tr>
-      <td data-label="name">Derek Jeter</td>
-      <td data-label="war">71.3</td>
-      <td data-label="ba">.310</td>
-      <td data-label="obp">.377</td>
-      <td data-label="slg">.440</td>
-      <td data-label="ops">.817</td>
-      <td data-label="rbi">1311</td>
-      <td data-label="hr">260</td>
-    </tr>
-      
-      <tr>
-      <td data-label="name">Cal Ripken Jr.</td>
-      <td data-label="war">95.9</td>
-      <td data-label="ba">.276</td>
-      <td data-label="obp">.340</td>
-      <td data-label="slg">.447</td>
-      <td data-label="ops">.788</td>
-      <td data-label="rbi">1695</td>
-      <td data-label="hr">431</td>
-    </tr>
-      
-      <tr>
-      <td data-label="name">Darryl Strawberry</td>
-      <td data-label="war">42.2</td>
-      <td data-label="ba">.259</td>
-      <td data-label="obp">.357</td>
-      <td data-label="slg">.505</td>
-      <td data-label="ops">.862</td>
-      <td data-label="rbi">1000</td>
-      <td data-label="hr">335</td>
-    </tr>
-  </tbody>
-  
-</table>
+        <table>
+
+          <thead>
+            <th>Name</th>
+            <th>Mobile No.</th>
+            <th>Semester</th>
+            <th>Branch</th>
+            <th>Attendence</th>
+            <th>Time</th>
+          </thead>
+
+          <tbody>
+            <?php
+            while ($userrow = mysqli_fetch_array($attdet)) {
+              // echo '<script type="text/javascript">
+              // window.location="dash.php";
+              // </script>';
+              if ($userrow) {
+
+
+
+                echo "<tr>";
+                echo '<td data-label="Name">' . $userrow["firstname"] . '</td>';
+                echo '<td data-label="Mobile No.">' . $userrow["mobileno"] . '</td>';
+                echo '<td data-label="Semester">' . $userrow["semester"] . '</td>';
+                echo '<td data-label="Branch">' . $userrow["branch"] . '</td>';
+                echo '<td data-label="Attendence"><span class="attstatus">CHECKED IN</span> </td>';
+                echo '<td data-label="Time">At 11:00 pm</td>';
+
+
+                echo "</tr>";
+              } else{
+                echo "<h1>No Users Checked In Yet</h1>";
+              }
+            }
+            ?>
+          </tbody>
+
+        </table>
 
       </div>
     </div>
